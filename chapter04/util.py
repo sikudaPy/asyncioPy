06f52596f49d27b1fd51d175386de8303e9bf8f7
mobilesikuda <ya.sikuda@yandex.ru>
2026-02-16 00:18:21 +0300

    up to 4 chapter

---
+++ chapter04/util.py
@@ -0,0 +1,22 @@
+import asyncio
+import functools
+import time
+from typing import Callable, Any
+
+async def delay(seconds: int):
+    await asyncio.sleep(seconds)
+
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f'выполняется {func} с аргументами {args} {kwargs}')
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f'{func} завершилась за {total:.4f} с')
+        return wrapped
+    return wrapper
